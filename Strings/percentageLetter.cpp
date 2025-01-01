f2e32d724a2d266f0c0cd4e8772b0f49bfcbf569
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-01-01 09:22:19 +0530

    Create percentageLetter.cpp

---
+++ Strings/percentageLetter.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int n = s.length();
+        int count = 0;
+        for (char ch: s) {
+            if (ch == letter) count++;
+        }
+        return (count * 100)/n;
+    }
+};
