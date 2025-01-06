1383eec1fe734a3fbfcf596c40cacf957dca848c
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-01-06 19:48:58 +0530

    Create DeepestLeavesSum.cpp

---
+++ Binary Tree/DeepestLeavesSum.cpp
@@ -0,0 +1,39 @@
+// 1302. Deepest Leaves Sum
+// Given the root of a binary tree, return the sum of values of its deepest leaves. 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            if (!q.empty()) {
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+};
