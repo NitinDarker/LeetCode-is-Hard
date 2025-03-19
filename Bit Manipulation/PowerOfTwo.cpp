41a2e22981a4a5076beea460e6557869c15b1a24
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-03-19 18:42:34 +0530

    231. Power of Two

---
+++ Bit Manipulation/PowerOfTwo.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/power-of-two/description/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n & (n-1)) return false;
+        return true;
+    }
+};
