994bb8e7c3a786a2e1b0ed032411a6ed97d27113
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-06-30 10:23:33 +0530

    1312. Minimum Insertion Steps to Make a String Palindrome
    DP-29

---
+++ Dynamic Programming/MinInsertionsToPalindrome.cpp
@@ -0,0 +1,30 @@
+/// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    // Longest Common Subsequence
+    int lcs(string text1, string text2) {
+        int n = text1.length(), m = text2.length();
+        vector<int> prev(m + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            vector<int> curr(m + 1, 0);
+            for (int j = 1; j <= m; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+
+    public:
+        int minInsertions(string s) {
+            int n = s.length();
+            string rev = s;
+            reverse(s.begin(), s.end());
+            return n-lcs(s, rev);
+        }
+    };
