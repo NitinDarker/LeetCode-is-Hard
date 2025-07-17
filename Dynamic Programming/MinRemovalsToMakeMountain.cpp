a2b62d4ff8a804b92422b903df36e1202e6aaf79
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-07-17 15:33:27 +0530

    1671. Minimum Number of Removals to Make Mountain Array
    DP-46

---
+++ Dynamic Programming/MinRemovalsToMakeMountain.cpp
@@ -0,0 +1,40 @@
+/// https://leetcode.com/problems/minimum-number-of-removals-to-make-mountain-array/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int LongestBitonicSequence(int n, vector<int> &nums) {
+        vector<int> leftLIS(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j  = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    leftLIS[i] = max(leftLIS[i], 1 + leftLIS[j]);
+                }
+            }
+        }
+        vector<int> rightLIS(n, 1);
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = n-1; j > i; j--) {
+                if (nums[j] < nums[i]) {
+                    rightLIS[i] = max(rightLIS[i], 1 + rightLIS[j]);
+                }
+            }
+        }
+        vector<int> bitonic(n, 0);
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            bitonic[i] = leftLIS[i] + rightLIS[i] - 1;
+            if (leftLIS[i] == 1 || rightLIS[i] == 1) {
+                bitonic[i] = 0;
+            }
+            maxLength = max(maxLength, bitonic[i]);
+        }
+        return maxLength;
+    }
+
+  public:
+    int minimumMountainRemovals(vector<int> &nums) {
+        int n = nums.size();
+        return n - LongestBitonicSequence(n, nums);
+    }
+};
