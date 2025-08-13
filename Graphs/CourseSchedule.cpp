5a7902bdeb90a139da3086dc28180179894a7b63
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-08-13 03:29:22 -0700

    207. Course Schedule
    G-24

---
+++ Graphs/CourseSchedule.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/course-schedule/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    vector<vector<int>> createGraph(int V, vector<vector<int>> &pre) {
+        vector<vector<int>> graph(V);
+        for (int i = 0; i < pre.size(); i++) {
+            graph[pre[i][1]].push_back(pre[i][0]);
+        }
+        return graph;
+    }
+
+    // Using Kahn's Algorithm
+    bool bfs(vector<vector<int>> &graph) {
+        int V = graph.size();
+        vector<int> inDegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (int nei : graph[i]) {
+                inDegree[nei]++;
+            }
+        }
+        queue<int> q;
+        vector<int> topo;
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0)
+                q.push(i);
+        }
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (int nei : graph[node]) {
+                inDegree[nei]--;
+                if (inDegree[nei] == 0)
+                    q.push(nei);
+            }
+        }
+        return (topo.size() == V);
+        // Returns true if no cycle
+    }
+
+  public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+        vector<vector<int>> graph = createGraph(numCourses, prerequisites);
+        return bfs(graph);
+    }
+};
