fa8809e8fa3158329a825e5f6f6f9e782d704690
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-09-08 17:59:37 +0530

    802. Find Eventual Safe States
    G-20 + G-25

--- Graphs/FindEventualSafeStates.cpp
+++ Graphs/FindEventualSafeStates.cpp
@@ -8,8 +8,10 @@ class Solution {
         vis[node] = true;
         pathVis[node] = true;
         for (int nei : graph[node]) {
-            if (!vis[nei] && DFS(nei, vis, pathVis, safe, graph, V)) return true;
-            else if (vis[nei] && pathVis[nei]) return true;
+            if (!vis[nei] && DFS(nei, vis, pathVis, safe, graph, V))
+                return true;
+            else if (vis[nei] && pathVis[nei])
+                return true;
         }
         safe[node] = true;
         pathVis[node] = false;
@@ -21,12 +23,63 @@ class Solution {
         int V = graph.size();
         vector<bool> vis(V, false), pathVis(V, false), safe(V, false);
         for (int i = 0; i < V; i++) {
-            if (!vis[i]) DFS(i, vis, pathVis, safe, graph, V);
+            if (!vis[i])
+                DFS(i, vis, pathVis, safe, graph, V);
         }
         vector<int> res;
         for (int i = 0; i < V; i++) {
-            if (safe[i]) res.push_back(i);
+            if (safe[i])
+                res.push_back(i);
         }
         return res;
+    }
+};
+
+// BFS -> Using Topological Sort (Kahn's Algorithm)
+class Solution {
+    vector<vector<int>> reverseGraph(vector<vector<int>> &graph) {
+        int V = graph.size();
+        vector<vector<int>> rev(V);
+        for (int i = 0; i < V; i++) {
+            for (int nei : graph[i]) {
+                rev[nei].push_back(i);
+            }
+        }
+        return rev;
+    }
+
+    vector<int> topoSort(vector<vector<int>> &adj) {
+        int V = adj.size();
+        vector<int> inDegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (int nei : adj[i]) {
+                inDegree[nei]++;
+            }
+        }
+        queue<int> q;
+        vector<int> safe;
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0)
+                q.push(i);
+        }
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe.push_back(node);
+            for (int nei : adj[node]) {
+                inDegree[nei]--;
+                if (inDegree[nei] == 0) q.push(nei);
+            }
+        }
+        return safe;
+    }
+
+  public:
+    vector<int> eventualSafeNodes(vector<vector<int>> graph) {
+        int V = graph.size();
+        vector<vector<int>> revGraph = reverseGraph(graph);
+        vector<int> safe = topoSort(revGraph);
+        sort(safe.begin(), safe.end());
+        return safe;
     }
 };
