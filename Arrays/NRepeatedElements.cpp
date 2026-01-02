04707bbcdd2ff74a83c2bb071b55a5a824fe21d7
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-01-02 22:23:21 +0530

    961. N-Repeated Element in Size 2N Array
    LeetCode Daily Challenge -> 02 Jan 2026

---
+++ Arrays/NRepeatedElements.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 2; i < n; i++) {
+            if (nums[i] == nums[i-1] || nums[i] == nums[i-2]) return nums[i];
+        }
+        return nums[0];
+    }
+};
