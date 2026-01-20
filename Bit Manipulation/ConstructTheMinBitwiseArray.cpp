109c65acac590f9e21d538dadd1512e4be421560
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-01-20 19:44:21 +0530

    3314. Construct the Minimum Bitwise Array I
    LeetCode Daily Challenge -> 20 Jan 2026

---
+++ Bit Manipulation/ConstructTheMinBitwiseArray.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/construct-the-minimum-bitwise-array-ii/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for (int &num : nums) {
+            if (!(num & 1)) {
+                ans.push_back(-1);
+                continue;
+            }
+            int j = 0;
+            while (true) {
+                int bit = ((num >> j) & 1);
+                if (bit == 0) break;
+                j++;
+            }
+            int x = (num & (~(1 << (j-1))));
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
