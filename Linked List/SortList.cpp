c93ad35ea3fd3a9e7d09df5ee1bd8d19b322d961
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-02-12 19:15:54 +0530

    148. Sort List
    LL-26

--- Linked List/SortList.cpp
+++ Linked List/SortList.cpp
@@ -1,51 +1,54 @@
+// https://leetcode.com/problems/sort-list/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
-    ListNode* findMiddle(ListNode* head) {
-        ListNode *slow = head, *fast = head;
-        while (fast->next != NULL && fast->next->next != NULL) {
+    ListNode *merge(ListNode *list1, ListNode *list2) {
+        ListNode *dHead = new ListNode(-1);
+        ListNode *p = list1, *q = list2, *merged = dHead;
+        while (p && q) {
+            if (p->val < q->val) {
+                merged->next = p;
+                p = p->next;
+            } else {
+                merged->next = q;
+                q = q->next;
+            }
+            merged = merged->next;
+        }
+        if (p) merged->next = p;
+        if (q) merged->next = q;
+        ListNode *mergedHead = dHead->next;
+        delete dHead;
+        return mergedHead;
+    }
+
+    ListNode *mergeSort(ListNode *head) {
+        if (!head || !head->next) return head;
+        ListNode *slow = head, *fast = head, *tail = nullptr;
+        while (fast && fast->next) {
+            tail = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
-        return slow;
+        tail->next = nullptr;
+        // Two LL -> head...tail , slow...fast
+        
+        ListNode *l1 = mergeSort(head);
+        ListNode *l2 = mergeSort(slow);
+        return merge(l1, l2);
     }
 
-    ListNode* merge(ListNode* leftList, ListNode* rightList) {
-        // Merge while sorting
-        ListNode* l = leftList;
-        ListNode* r = rightList;
-        ListNode* dNode = new ListNode(-1);
-        ListNode* temp = dNode;
-        while (l && r) {
-            if (l->val < r->val) {
-                temp->next = l;
-                l = l->next;
-            } else {
-                temp->next = r;
-                r = r->next;
-            }
-            temp = temp->next;
-        }
-        if (!l) {
-            temp->next = r;
-        }
-        if (!r) {
-            temp->next = l;
-        }
-        return dNode->next;
+  public:
+    ListNode *sortList(ListNode *head) {
+        return mergeSort(head);
     }
-
-    ListNode* mergeSort(ListNode* head) {
-        if (!head || !(head->next)) {
-            return head;
-        }
-        ListNode* middleNode = findMiddle(head);
-        ListNode *leftList = head, *rightList = middleNode->next;
-        middleNode->next = NULL;
-        leftList = mergeSort(leftList);
-        rightList = mergeSort(rightList);
-        head = merge(leftList, rightList);
-        return head;
-    }
-
-public:
-    ListNode* sortList(ListNode* head) { return mergeSort(head); }
 };
