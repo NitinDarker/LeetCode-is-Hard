a0b4bc03766b1e0ed6bbe921ff81c6c0a1edb70f
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-03-05 19:19:38 +0530

    Longest Substring with K Uniques

--- Sliding Window & Two Pointer/LongestSubstringWithK.cpp
+++ Sliding Window & Two Pointer/LongestSubstringWithK.cpp
@@ -1,26 +1,25 @@
-/// https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+// https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
   public:
     int longestKSubstr(string &s, int k) {
         int n = s.length();
-        unordered_map<char, int> freq;
-        int maxLength = -1;
+        vector<int> freq(26, 0);
+        int dist = 0, size = -1;
         int i = 0, j = 0;
         while (j < n) {
-            freq[s[j]]++;
-            if (freq.size() < k) {
-                j++;
-            } else if (freq.size() == k) {
-                maxLength = max(maxLength, j-i+1);
-                j++;
-            } else {
-                freq[s[i]]--;
-                if (freq[s[i]] == 0) freq.erase(s[i]);
+            int ch = s[j] - 'a';
+            if (freq[ch]++ == 0) dist++;
+            while (dist > k) {
+                int prev = s[i] - 'a';
+                if (--freq[prev] == 0) dist--;
                 i++;
-                freq[s[j]]--; // Counter Measure
             }
-            
+            j++;
+            if (dist == k) size = max(size, j-i);
         }
-        return maxLength;
+        return size;
     }
 };
