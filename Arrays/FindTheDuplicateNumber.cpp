8f4048dd2858744b2ccd520ae2548413e39ddd4b
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-03-20 14:36:42 +0530

    287. Find the Duplicate Number

---
+++ Arrays/FindTheDuplicateNumber.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+// Using Floyd's Cycle Detection
+class Solution {
+  public:
+    int findDuplicate(vector<int> &nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
