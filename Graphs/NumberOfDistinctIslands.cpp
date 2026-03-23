646ae29ea34c1c63dcd189c8b29494501c0e4eae
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-03-23 23:04:15 +0530

    Number of Distinct Islands

--- Graphs/NumberOfDistinctIslands.cpp
+++ Graphs/NumberOfDistinctIslands.cpp
@@ -3,33 +3,38 @@
 using namespace std;
 
 class Solution {
-    const vector<int> dx = {1, 0, -1, 0};
-    const vector<int> dy = {0, 1, 0, -1};
-    
-    void dfs(int x, int y, vector<vector<bool>> &vis, const vector<vector<int>> &grid, set<pair<int,int>> &arr, const pair<int,int> &base) {
+    // left-down-right-up
+    vector<int> dirX = {-1, 0, 1, 0};
+    vector<int> dirY = {0, -1, 0, 1};
+
+    void DFS(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &vis, vector<pair<int, int>> &isl, pair<int, int> &base) {
         int n = grid.size(), m = grid[0].size();
-        if (x < 0 || x >= n || y < 0 || y >= m || vis[x][y] || grid[x][y] == 0) return;
-        vis[x][y] = 1;
-        int i = x-base.first, j = y-base.second;
-        arr.insert({i, j});
-        for (int i = 0; i < 4; i++) dfs(x+dx[i], y+dy[i], vis, grid, arr, base);
+        vis[i][j] = 1;
+        isl.push_back({i - base.first, j - base.second});
+        for (int k = 0; k < 4; k++) {
+            int x = i + dirX[k];
+            int y = j + dirY[k];
+            if (x >= 0 && x < n && y >= 0 && y < m && !vis[x][y] && grid[x][y] == 1) {
+                DFS(x, y, grid, vis, isl, base);
+            }
+        }
     }
-    
-    public:
+
+  public:
     int countDistinctIslands(vector<vector<int>> &grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool> (m, false));
-        set<set<pair<int,int>>> isl;
+        set<vector<pair<int, int>>> islands;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1 && !vis[i][j]) {
-                    set<pair<int,int>> arr;
-                    pair<int,int> base = {i, j};
-                    dfs(i, j, vis, grid, arr, base);
-                    isl.insert(arr);
+                    vector<pair<int, int>> isl;
+                    pair<int, int> base = {i, j};
+                    DFS(i, j, grid, vis, isl, base);
+                    islands.insert(isl);
                 }
             }
         }
-        return isl.size();
+        return islands.size();
     }
 };
