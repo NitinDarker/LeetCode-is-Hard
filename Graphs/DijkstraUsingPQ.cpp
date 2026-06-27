53e562c90443f2f5c6cba8062332a29d473ab92a
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2026-06-27 19:25:37 +0530

    Dijkstra Algorithm (Priority Queue)
    G-32

--- Graphs/DijkstraUsingPQ.cpp
+++ Graphs/DijkstraUsingPQ.cpp
@@ -5,30 +5,30 @@ using namespace std;
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        unordered_map<int, vector<pair<int, int>>> graph; // node -> [{node, cost}]
-        for (auto &edg : edges) {
-            int u = edg[0], v = edg[1], w = edg[2];
-            graph[u].push_back({v, w});
-            graph[v].push_back({u, w}); // Undirected graph
+        vector<vector<pair<int, int>>> graph(V);
+        for (auto &edge : edges) {
+            int u = edge[0], v = edge[1], c = edge[2];
+            graph[u].push_back({v, c});
+            graph[v].push_back({u, c});
         }
 
-        // [cost, node]
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        // pq <- {cost, node}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> dist(V, 1e9);
-
-        heap.push({0, src});
+        pq.push({0, src});
         dist[src] = 0;
 
-        while (!heap.empty()) {
-            int cur = heap.top().second, curCost = heap.top().first;
-            heap.pop();
-            if (curCost > dist[cur]) continue;
-            for (auto &nei : graph[cur]) {
-                int nextNode = nei.first, nextCost = nei.second;
-                int totalCost = nextCost + curCost;
-                if (dist[nextNode] > totalCost) {
-                    dist[nextNode] = totalCost;
-                    heap.push({totalCost, nextNode});
+        while (!pq.empty()) {
+            auto [cost, node] = pq.top();
+            pq.pop();
+
+            // Lazy pruning
+            if (cost > dist[node]) continue;
+
+            for (auto &[nei, weight] : graph[node]) {
+                if (cost + weight < dist[nei]) {
+                    dist[nei] = cost + weight;
+                    pq.push({cost + weight, nei});
                 }
             }
         }
